c424532f835b75d13d98434b938db01fffe966ec
92thunder <ryota.kunisada@techtouch.co.jp>
2021-03-28 21:08:25 +0900

    add AppBar

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Toolbar, AppBar, Typography } from '@material-ui/core'
+
+export const Header: React.VFC = () => {
+  return (
+    <StyledAppBar>
+      <Toolbar>
+        <Typography variant="h6">
+          つくりおき
+        </Typography>
+      </Toolbar>
+    </StyledAppBar>
+  )
+}
+
+const StyledAppBar = styled(AppBar)`
+`
