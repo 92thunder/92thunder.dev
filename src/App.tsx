fc3f2381ab939aa7f09289497de30780f7fe47ad
92thunder <ryota.kunisada@techtouch.co.jp>
2021-03-28 21:16:06 +0900

    remove  App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import './App.css';
 import { Header } from './components/Header';
 
-function App() {
+export const App: React.VFC = () => {
   return (
     <div className="App">
       <Header></Header>
     </div>
-  );
+  )
 }
-
-export default App;
