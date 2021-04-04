dc859227b9a9be51443cfe9f5221ba69fb31f871
ryota.kunisada <r.kunisada661@gmail.com>
2021-04-05 00:10:04 +0900

    add github link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Toolbar, AppBar, Typography } from '@material-ui/core'
+import { Toolbar, AppBar, Typography, IconButton } from '@material-ui/core'
+import { GitHub } from '@material-ui/icons'
 
 export const Header: React.VFC = () => {
+  const handleClickGithub = () => {
+    window.location.href = 'https://github.com/92thunder/blog'
+  }
   return (
     <StyledAppBar position="static">
       <Toolbar variant="dense">
-        <Typography variant="h6">
+        <Title variant="h6">
           92thunder.dev
-        </Typography>
+        </Title>
+        <IconButton color="inherit" onClick={handleClickGithub}>
+          <GitHub/>
+        </IconButton>
       </Toolbar>
     </StyledAppBar>
   )
 }
 
 const StyledAppBar = styled(AppBar)`
+`
+
+const Title = styled(Typography)`
+  flex: 1;
 `
