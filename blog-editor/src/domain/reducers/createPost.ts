caa6d17ded11100faef8e29e46abe58e4dc80c2b
ryota.kunisada <r.kunisada661@gmail.com>
2022-04-18 00:49:58 +0900

    refactor: replace uuid -> crypto.randomUUID

--- blog-editor/src/domain/reducers/createPost.ts
+++ blog-editor/src/domain/reducers/createPost.ts
@@ -1,9 +1,8 @@
 import { Post } from '../../types'
-import { v4 as uuidv4 } from 'uuid'
 
 export const createPost: () => Post = () => {
   return {
-    id: uuidv4(),
+    id: crypto.randomUUID(),
     title: '',
     body: '',
     published_at: new Date(),
