38998064240edd0b774235a3006206e7e7c54130
ryota.kunisada <r.kunisada661@gmail.com>
2022-12-09 02:00:28 +0900

    １記事追加

--- posts/info.ts
+++ posts/info.ts
@@ -7,6 +7,12 @@ type PostInfo= {
 
 export const postsInfo: PostInfo[] = [
 	{
+		id: '8e3e28ed-2b71-4900-a6eb-838e2c48fec2',
+		title: '2022年の抱負',
+		mdFilename: '2022-01-15.md',
+		publishedAt: (new Date('2022/01/15')).toString()
+	},
+	{
 		id: 'test',
 		title: 'Test',
 		mdFilename: 'test.md',
