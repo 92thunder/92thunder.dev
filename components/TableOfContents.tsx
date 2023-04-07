09d9c7e13daf03367d0c83f5f783260f6f981a22
ryota.kunisada <r.kunisada661@gmail.com>
2023-04-07 23:20:20 +0900

    :iphone: adjust min-width for table of content

--- components/TableOfContents.tsx
+++ components/TableOfContents.tsx
@@ -1,38 +1,38 @@
 import { FC } from "react"
 import { getHeadings } from "../libs/getHeadings"
-import { Card, CardContent, Link, List, ListItem } from "@mui/material"
+import { Card, Link, List, ListItem } from "@mui/material"
 import styled from "@emotion/styled"
 
 export const TableOfContents: FC<{ markdown: string }> = ({ markdown }) => {
 	const headings = getHeadings(markdown)
 	return (
 		<StyledCard>
-			<CardContent sx={{ padding: 0 }}>
-				<nav>
-					<List>
-						{headings.map((heading) => (
-							<ListItem key={heading.id}>
-								<Link fontSize={12} href={`#${heading.id}`}>
-									{heading.text}
-								</Link>
-							</ListItem>
-						))}
-					</List>
-				</nav>
-			</CardContent>
+			<nav>
+				<List>
+					{headings.map((heading) => (
+						<ListItem key={heading.id}>
+							<Link fontSize={12} href={`#${heading.id}`}>
+								{heading.text}
+							</Link>
+						</ListItem>
+					))}
+				</List>
+			</nav>
 		</StyledCard>
 	)
 }
 
 const StyledCard = styled(Card)`
-	min-width: 20%;
+	min-width: 10rem;
+	width: 100%;
+	max-width: 20%;
 	position: sticky;
 	top: 2rem;
 	height: min-content;
 	max-height: calc(100vh - 4rem);
-	overflow-y: scroll;
+	overflow-y: auto;
 
-	@media (max-width: 600px) {
+	@media (max-width: 640px) {
 		display: none;
 	}
 `
