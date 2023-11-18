51dae3d9c12ba93d0c021988d2876ee594a14c19
ryota.kunisada <r.kunisada661@gmail.com>
2023-11-18 23:59:34 +0900

    :white_check_mark: Fix test.

--- libs/getHeadings.spec.ts
+++ libs/getHeadings.spec.ts
@@ -7,14 +7,17 @@ test('getHeadings', () => {
 `
 	expect(getHeadings(markdown)).toEqual([
 		{
+			id: 'Heading1',
 			level: 1,
 			text: 'Heading 1'
 		},
 		{
+			id: 'Heading2',
 			level: 2,
 			text: 'Heading 2'
 		},
 		{
+			id: 'Heading3',
 			level: 3,
 			text: 'Heading 3'
 		},
